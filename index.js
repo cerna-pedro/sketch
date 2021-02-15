58e87ec400171f304a4c86b05747ca15237a52b1
Pedro Cerna <pedro.d.cerna@gmail.com>
2021-02-14 21:57:03 -0600

    Fix the fact that event fires more than once when I exit popup.

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ const popup = document.querySelector('.popup');
 const numberInput = document.querySelector('#number-input');
 
 const clearGrid = (e) => {
-  toggleClassOff()
+  toggleClassOff();
   createNewGrid(e);
 };
 
 const toggleClassOff = () => {
   popup.classList.remove('hidden');
   formContainer.classList.remove('hidden');
+  setTimeout(() => {
+    numberInput.focus();
+  }, 20);
 };
 
 const toggleClassOn = () => {
   popup.classList.add('hidden');
   formContainer.classList.add('hidden');
 };
-
 
 const checkNumberRange = () => {
   if (numberInput.value > 30) {
@@ -32,33 +34,69 @@ const checkNumberRange = () => {
 };
 
 const validateEntry = (e) => {
-  const possible = '0123456789Backspace';
+  const possible = '0123456789BackspaceEnter';
   if (!possible.includes(e.key)) {
     e.preventDefault();
   }
 };
 
-const createNewGrid = (e) => {
-  numberInput.value = ''
-  squareForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const numberOfSquares = parseInt(e.target[0].value);
-    console.log(numberOfSquares);
-    toggleClassOn()
-    createGrid(numberOfSquares);
-  });
-};
-clearButton.addEventListener('pointerdown', clearGrid);
-numberInput.addEventListener('keydown', validateEntry);
-numberInput.addEventListener('change', checkNumberRange);
-
-const wait = (amount = 0) =>
-  new Promise((resolve) => setTimeout(resolve, amount));
-
-const createGrid = async (num = 16) => {
-  while (gridContainer.firstChild) {
-    gridContainer.removeChild(gridContainer.firstChild);
+const exitPopup = (e) => {
+  if (e.target === popup || e.key === 'Escape') {
+    toggleClassOn();
   }
+};
+
+const changeColor = (e) => {
+  console.log(e.target.style.backgroundColor);
+  if (!e.target.style.backgroundColor) {
+    const randomRed = Math.round(Math.random() * 255);
+    const randomGreen = Math.round(Math.random() * 255);
+    const randomBlue = Math.round(Math.random() * 255);
+    e.target.style.backgroundColor = `rgba(${randomRed},${randomGreen},${randomBlue},.3)`;
+  } else {
+    current_color = getComputedStyle(e.target).getPropertyValue('background-color');
+    match = /rgba?\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*(,\s*\d+[\.\d+]*)*\)/g.exec(
+      current_color
+      );
+      if (!match[4]) {
+        return
+
+      }
+      const a = parseFloat(match[4].replace(', ', ''))+.1;
+      console.log(a);
+      e.target.style.backgroundColor =
+      'rgba(' + [match[1], match[2], match[3], a].join(',') + ')';
+    }
+  };
+
+  const createNewGrid = (e) => {
+    numberInput.value = '';
+    console.log(e);
+    squareForm.addEventListener(
+      'submit',
+      (e) => {
+        e.preventDefault();
+        const numberOfSquares = parseInt(e.target[0].value);
+        console.log(numberOfSquares);
+        toggleClassOn();
+        createGrid(numberOfSquares);
+      },
+      { once: true }
+      );
+    };
+    clearButton.addEventListener('pointerdown', clearGrid);
+    numberInput.addEventListener('keydown', validateEntry);
+    numberInput.addEventListener('change', checkNumberRange);
+    popup.addEventListener('pointerdown', exitPopup);
+    popup.addEventListener('keydown', exitPopup);
+
+    const wait = (amount = 0) =>
+    new Promise((resolve) => setTimeout(resolve, amount));
+
+    const createGrid = async (num = 10) => {
+      while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+      }
   let i = 0;
   while (i < num) {
     const gridRow = document.createElement('div');
@@ -73,6 +111,12 @@ const createGrid = async (num = 16) => {
     gridContainer.append(gridRow);
     i++;
   }
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell) => {
+    cell.addEventListener('pointerover', changeColor);
+    // cell.addEventListener('pointerdown', changeColor);
+
+  });
 };
 
 createGrid();
